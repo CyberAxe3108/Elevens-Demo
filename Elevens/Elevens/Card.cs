7fae1c6e1033d8336d0d403e935d54132aa39167
CyberAxe3108 <125237722+CyberAxe3108@users.noreply.github.com>
2026-02-24 15:03:33 -0500

    Refactor Card class to use properties and enums

--- Elevens/Elevens/Card.cs
+++ Elevens/Elevens/Card.cs
@@ -1,21 +1,32 @@
-﻿public class Card
+namespace Elevens;
+
+public class Card
 {
-   private int value;
-   private string suit;
- 
- public static string[] suits = { "Hearts", "Diamonds", "Clubs", "Spades" };
- public Card(int value, string suit)
- {
-     this.value = value;
-     this.suit = suit;
- }
-    public int GetValue()
+    public int Rank { get; }
+    public Suit Suit{get;}
+    
+    
+    public int ValueForEleven => Rank <= 10 ? Rank : 0; 
+    public bool IsJack => Rank == 11;
+    public bool IsQueen => Rank == 12;
+    public bool IsKing => Rank == 13;
+
+    public Card(int rank, Suit suit)
     {
-        return value;
+        Rank = rank;
+        Suit = suit;
     }
 
     public override string ToString()
     {
-        return $"{value} of {suit}";
+        string rankStr = Rank switch
+        {
+            1 => "Ace",
+            11 => "Jack",
+            12 => "Queen",
+            13 => "King",
+            _ => Rank.ToString()
+        };
+        return $"{rankStr} of {Suit}";
     }
 }
